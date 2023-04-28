bca68fb220bb04d4c247de4dfe8dfc72429a0f97
Haseeb Majid <hello@haseebmajid.dev>
2023-04-28 23:33:41 +0100

    some werk

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+map("x", "<leader>p", "\"_dP", { desc = "Paste without updating register" })
+
+
+map("v", "<", "<gv", { desc = "Stay in visual mode during outdent" })
+map("v", ">", ">gv", { desc = "Stay in visual mode during indent" })
+map("v", "J", ":m >+1<CR>gv=gv", { desc = "Mouse selected lines down" })
+map("v", "K", ":m >-2<CR>gv=gv", { desc = "Mouse selected lines up" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+opt.timeoutlen = 100
+opt.hlsearch = false
+opt.incsearch = true
+opt.colorcolumn = "120"
