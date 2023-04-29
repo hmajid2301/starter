9a754285aa1e358846071b763de4668c416c6681
Haseeb Majid <hello@haseebmajid.dev>
2023-04-29 17:37:51 +0100

    some werk

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -37,15 +37,16 @@ return {
       },
     },
   },
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-    },
-  }
+  -- {
+  --   "utilyre/barbecue.nvim",
+  --   name = "barbecue",
+  --   version = "*",
+  --   dependencies = {
+  --     "SmiteshP/nvim-navic",
+  --     "nvim-tree/nvim-web-devicons", -- optional dependency
+  --   },
+  --   opts = {
+  --     theme = "catppuccin",
+  --   },
+  -- }
 }
