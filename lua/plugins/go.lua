cd9d316509df06d465d1211998d2f1ea3efefca9
Haseeb Majid <hello@haseebmajid.dev>
2023-05-04 23:14:28 +0100

    Update go format

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,28 +1,40 @@
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require("go.format").goimport()
+  end,
+  group = format_sync_grp,
+})
+
 return {
-	{
-		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("go").setup()
-		end,
-		event = { "CmdlineEnter" },
-		ft = { "go", 'gomod' },
-		build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
-	},
-	{
-		"nvim-neotest/neotest",
-		dependencies = { -- optional packages
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-neotest/neotest-go",
-			"nvim-neotest/neotest-python",
-			"marilari88/neotest-vitest",
-		},
-		opts = {},
-	},
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = { -- optional packages
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-python",
+      "marilari88/neotest-vitest",
+    },
+    opts = {
+      goimport = "goimport",
+      gofmt = "goimports",
+    },
+  },
 }
