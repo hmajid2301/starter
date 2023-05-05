5473f02bccfbcfc7dede7c149463debf9d6248d6
Haseeb Majid <hello@haseebmajid.dev>
2023-05-05 12:17:08 +0100

    Update telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,8 +21,8 @@ vim.keymap.set(
   { desc = "Change directory using zoxide" }
 )
 vim.keymap.set("n", "<leader>uu", "<cmd>Telescope undo<cr>", { desc = "Show undoo tree" })
-vim.keymap.set("n", "<A-H>", "^", { desc = "Move to first character in line" })
-vim.keymap.set("n", "<A-L>", "$", { desc = "Move to last character in line" })
+vim.keymap.set("n", "<C-H>", "^", { desc = "Move to first character in line" })
+vim.keymap.set("n", "<C-L>", "$", { desc = "Move to last character in line" })
 
 -- Keep matches center screen when cycling
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Keep cursor in middle when jumping" })
@@ -35,12 +35,29 @@ vim.keymap.set("n", "N", "Nzzzv", { desc = "Back search '/' or '?'" })
 vim.keymap.set("n", "<leader>tt", function()
   Util.float_term({ "gdu" }, { cwd = Util.get_root(), esc_esc = false })
 end, { desc = "Toggle: Go Disk Usage" })
--- vim.keymap.map("n", "<leader>tu", function()
---   Util.float_term({ "btm" }, { cwd = Util.get_root(), esc_esc = false })
--- end, { desc = "Toggle: Bottom" })
+vim.keymap.set("n", "<leader>tu", function()
+  Util.float_term({ "btm" }, { cwd = Util.get_root(), esc_esc = false })
+end, { desc = "Toggle: Bottom" })
 
 vim.keymap.set("n", "<A-J>", "mzJ`z", { desc = "combine with line up" })
 
 -- Newlines
 vim.keymap.set("n", "<leader>o", 'o<Esc>0"_D', { desc = "Create a new line below without leaving normal mode" })
 vim.keymap.set("n", "<leader>O", 'O<Esc>0"_D', { desc = "Create a new line above without leaving normal mode" })
+
+-- Tmux
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<C-h>", "<cmd>lua require'tmux'.move_left()<cr>", { desc = "Go to left window" })
+map("n", "<C-j>", "<cmd>lua require'tmux'.move_bottom()<cr>", { desc = "Go to lower window" })
+map("n", "<C-k>", "<cmd>lua require'tmux'.move_top()<cr>", { desc = "Go to upper window" })
+map("n", "<C-l>", "<cmd>lua require'tmux'.move_right()<cr>", { desc = "Go to right window" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,16 +6,25 @@ return {
       build = "make",
       config = function()
         require("telescope").load_extension("fzf")
-        require("telescope").load_extension("undo")
-        require("telescope").load_extension("zoxide")
       end,
       "jvgrootveld/telescope-zoxide",
       "debugloop/telescope-undo.nvim",
     },
+    config = function()
+      require("telescope").load_extension("undo")
+      require("telescope").load_extension("zoxide")
+    end,
     opts = function(_, opts)
       opts.extensions = {
         zoxide = {
           prompt_title = " Projects List",
+        },
+        undo = {
+          side_by_side = true,
+          layout_strategy = "vertical",
+          layout_config = {
+            preview_height = 0.8,
+          },
         },
       }
     end,
