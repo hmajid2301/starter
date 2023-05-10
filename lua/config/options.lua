148aa9c2d68d4ec6352334f46d32e46ed205d9dd
Haseeb Majid <hello@haseebmajid.dev>
2023-05-10 13:05:46 +0100

    misc

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.mapleader = " "
 
 vim.opt.timeoutlen = 100
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.colorcolumn = "120"
 vim.opt.swapfile = false
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
 
 -- Highlight groups for illuminate
 vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
---
+++ lua/plugins/ensure_installed.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "c",
+        "css",
+        "bash",
+        "dockerfile",
+        "go",
+        "gomod",
+        "gosum",
+        "fish",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "python",
+        "svelte",
+        "sql",
+        "terraform",
+        "typescript",
+        "vim",
+        "yaml",
+      })
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "golangci_lint_ls",
+        "gopls",
+        "jsonls",
+        "lua_ls",
+        "tsserver",
+      })
+    end,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      vim.list_extend(opts.ensure_installed, {
+        "eslint_d",
+        "flake8",
+        "golangci-lint",
+        "black",
+        "goimports",
+        "shfmt",
+        "stylua",
+      })
+    end,
+  },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      vim.list_extend(opts.ensure_installed, {
+        "debugpy",
+        "delve",
+        "go-debug-adapter",
+      })
+    end,
+  },
+}
